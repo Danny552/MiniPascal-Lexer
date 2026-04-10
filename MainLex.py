69ad6a3e9572e6199b3717e3715a7683b77d28c6
Danny552 <danielhenao552@gmail.com>
2026-04-10 08:15:42 -0500

    added ASSINGN token

--- MainLex.py
+++ MainLex.py
@@ -7,7 +7,7 @@ with open ('Tokens.json', 'r') as Tokens:
 tokens = [
     "PLUS", "MINUS", "TIMES", "DIVIDE",
     "LPAREN", "RPAREN", "EQUALS", "SEMICOLON", "COLON", "COMMA", "DOT",
-    "LBRACKET", "RBRACKET", "SINGLE_COMMENT"
+    "LBRACKET", "RBRACKET", "SINGLE_COMMENT", "ASSIGN"
 ] + list(dict_tokens.values())
 
 #Definición de tokens básicos, operadores y símbolos
@@ -18,6 +18,7 @@ t_DIVIDE = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_EQUALS = r'='
+t_ASSIGN = r':='
 t_SEMICOLON = r';'
 t_COLON = r':'
 t_COMMA = r','
